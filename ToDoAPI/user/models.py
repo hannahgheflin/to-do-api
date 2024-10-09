f9a9ccddcabce948074a939d08c5306a8fadc78c
Hannah Heflin <131992199+hannahgheflin@users.noreply.github.com>
2024-10-09 09:28:37 -0500

    Update models.py with list and item models

--- ToDoAPI/user/models.py
+++ ToDoAPI/user/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# List class
+class List(models.Model):
+    title = models.CharField(max_length=100)
 
-    
+    def __str__(self):
+        return self.title
+
+# List Item Class
 class Task(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    description = models.TestField(null=True, black=True)
     status = models.BooleanField()
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
